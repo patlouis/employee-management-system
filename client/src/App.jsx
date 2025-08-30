4b5fc2a987eadb24e973abc7413638219643b620
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-08-30 14:02:31 +0800

    fix: update App.jsx routes of Login/Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 import AdminDashboard from './pages/AdminDashboard';
 import Employees from './pages/Employees';
 import Departments from './pages/Departments';
